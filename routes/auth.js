5084ba1fd3a59a60ae9c61096b85cc70c536a8f3
Habeeb Kareem <olaidekareem43@yahoo.com>
2025-04-20 08:58:00 +0100

    add get currently logged in user route

--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,13 @@
 const express = require('express');
-const {register, login} = require('../controllers/auth');
+const {register, login, getMe} = require('../controllers/auth');
 
 const router = express.Router();
 
+// Protect auth middleware
+const {protect} = require('../middleware/auth');
+
 router.post('/register', register);
 router.post('/login', login);
+router.route('/me').get(protect, getMe);
 
 module.exports = router;
