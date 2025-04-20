0acfe97b2c53a9fdd0c863bf2d592e369d1f629c
Habeeb Kareem <olaidekareem43@yahoo.com>
2025-04-20 06:50:19 +0100

    connect app to db
    add logging in development env

--- server.js
+++ server.js
@@ -1,14 +1,26 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const morgan = require('morgan');
+const colors = require('colors');
+
+const connectDB = require('./config/db');
 
 // load env vars
 dotenv.config({path: './config/config.env'});
+
+// Connect to database
+connectDB();
 
 // Initialize App
 const app = express();
 
 // Route files
 const auth = require('./routes/auth');
+
+// Dev logging middleware (morgan)
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'));
+}
 
 // Router
 app.use('/api/v1/auth', auth);
