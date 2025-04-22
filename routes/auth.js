2c9900501dbbe69b2582f31faac411c7c8a5a116
Habeeb Kareem <olaidekareem43@yahoo.com>
2025-04-22 07:17:53 +0100

    add forgotpassword and resetpassword routes

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,12 @@
 const express = require('express');
-const {register, login, getMe, logout} = require('../controllers/auth');
+const {
+    register,
+    login,
+    getMe,
+    logout,
+    forgotPassword,
+    resetPassword,
+} = require('../controllers/auth');
 
 const router = express.Router();
 
@@ -10,5 +17,7 @@ router.post('/register', register);
 router.post('/login', login);
 router.route('/me').get(protect, getMe);
 router.get('/logout', logout);
+router.post('/forgotpassword', forgotPassword);
+router.put('/resetpassword/:resettoken', resetPassword);
 
 module.exports = router;
